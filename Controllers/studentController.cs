d2f99bb2cede27a0ac1feb82833f451cc7b40f6e
u24623815 <u24623815@RAST-001>
2025-03-09 20:28:27 +0200

    new

--- Controllers/studentController.cs
+++ Controllers/studentController.cs
@@ -14,11 +14,11 @@ namespace Prac1.Controllers
         {
 
             List<Models.StudentModel> student = new List<Models.StudentModel>();
-            student.Add(new Models.StudentModel { StudentNumber = "24623815", Name = "Kamohelo", Surname = "Nkomo", Email = "u24623815@tuks.co.za" });
-            student.Add(new Models.StudentModel { StudentNumber = "24579280", Name = "Arielle", Surname = "Tshiswaka", Email = "u2479280@tuks.co.za" });
-            student.Add(new Models.StudentModel { StudentNumber = "21233815", Name = "Boitumelo", Surname = "Ngara", Email = "u21233815@tuks.co.za" });
-            student.Add(new Models.StudentModel { StudentNumber = "24623246", Name = "Bandile", Surname = "Brown", Email = "u246238246@tuks.co.za" });
-            student.Add(new Models.StudentModel { StudentNumber = "23623819", Name = "Kamogelo", Surname = "Kobese", Email = "u23623819@tuks.co.za" });
+            student.Add(new Models.StudentModel { StudentNumber = "24623815", Name = "Kamohelo", Surname = "Nkomo", Email = "u24623815@tuks.co.za", myLink="~/HTML/Kamohelo.html"});
+            student.Add(new Models.StudentModel { StudentNumber = "24579280", Name = "Arielle", Surname = "Tshiswaka", Email = "u2479280@tuks.co.za" , myLink="~/HTML/Arielle.html"});
+            student.Add(new Models.StudentModel { StudentNumber = "21233815", Name = "Lynn", Surname = "Ngara", Email = "u21233815@tuks.co.za" , myLink="~/HTML/Lynn.html"});
+            student.Add(new Models.StudentModel { StudentNumber = "24623246", Name = "Pietru", Surname = "Brown", Email = "u246238246@tuks.co.za", myLink="~/HTML/Pietrus.html" });
+            student.Add(new Models.StudentModel { StudentNumber = "23623819", Name = "Koketso", Surname = "Kobese", Email = "u23623819@tuks.co.za" , myLink="~/HTML/Koketso.html"});
             return View(student);
         }
     }
