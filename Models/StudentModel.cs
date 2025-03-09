c56a681aef83e75eaf8526dc8ce640f1edafbf79
u24623815 <u24623815@RAST-001>
2025-03-09 19:50:05 +0200

    pietrus

--- Models/StudentModel.cs
+++ Models/StudentModel.cs
@@ -8,16 +8,32 @@ namespace Prac1.Models
 {
 	public class StudentModel
 	{
-		[Display (Name = "STUDENT NUMBER")]//  add as a decorater
-		public string StudentNumber { get; set; }
+		//[Display (Name = "STUDENT NUMBER")] 
+		//public string StudentNumber { get; set; }
 
-        [Display(Name = "NAME")] //Add as decorator
-        public string Name { get; set; }
+       // [Display(Name = "NAME")] //Add as decorator
+        //public string Name { get; set; }
 
-        [Display(Name = "SURNAME")]//  add as a decorater
-        public string Surname { get; set; }
+        //[Display(Name = "SURNAME")]//  add as a decorater
+        //public string Surname { get; set; }
 
-        [Display(Name = "EMAIL ADDRESS")]//  add as a decorater
-        public string Email{ get; set; }
+        //[Display(Name = "EMAIL ADDRESS")]//  add as a decorater
+        //public string Email{ get; set; }
+
+        //code from code example model
+
+
+         [Display(Name = "First Name")]
+        public string FirstName { get; set; }
+
+        [Display(Name = "Last Name")]
+        public string LastName { get; set; }
+
+        [Display(Name = "Current Age")]
+        public int Age { get; set; } = 0;
+
+        [Display(Name = "Living Status")]
+        public bool IsAlive { get; set; } = true;
+
     }
 }
